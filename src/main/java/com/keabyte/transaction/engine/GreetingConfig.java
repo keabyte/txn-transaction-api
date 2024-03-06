3b395584e6aa87c4e3d6e2bb6287e69b8605d65f
Taylar <keast.taylar@gmail.com>
2024-03-07 00:12:22 +1100

    initial commit

---
+++ src/main/java/com/keabyte/transaction/engine/GreetingConfig.java
@@ -0,0 +1,12 @@
+package com.keabyte.transaction.engine;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
